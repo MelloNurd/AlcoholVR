11d9abf8eb6b7d618d20b3205116184481afdc30
Davin Lewis <102100872+davinlewis@users.noreply.github.com>
2025-06-09 14:09:19 -0700

    added bottle audio

--- Assets/BottleCap.cs
+++ Assets/BottleCap.cs
@@ -5,18 +5,25 @@ public class BottleCap : MonoBehaviour
 {
     XRGrabInteractable grabInteractable;
     FixedJoint fixedJoint;
+    AudioSource audioSource;
 
     private void Awake()
     {
         grabInteractable = GetComponent<XRGrabInteractable>();
         fixedJoint = GetComponent<FixedJoint>();
+        audioSource = GetComponent<AudioSource>();
     }
 
     private void Update()
     {
-        if(grabInteractable.isSelected)
+        if(grabInteractable.isSelected && grabInteractable != null)
         {
             fixedJoint.breakTorque = 5f;
         }
+    }
+
+    private void OnJointBreak(float breakForce)
+    {
+        audioSource.Play();
     }
 }
