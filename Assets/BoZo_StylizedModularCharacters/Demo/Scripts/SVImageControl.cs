b7a9e86709f7e9bbbf40f784b40ad0114135ed48
Davin Lewis <102100872+davinlewis@users.noreply.github.com>
2025-07-15 12:39:32 -0700

    updated character creator to work with color picker

--- Assets/BoZo_StylizedModularCharacters/Demo/Scripts/SVImageControl.cs
+++ Assets/BoZo_StylizedModularCharacters/Demo/Scripts/SVImageControl.cs
@@ -6,18 +6,14 @@ using UnityEngine.EventSystems;
 
 namespace Bozo.ModularCharacters
 {
-
-
     public class SVImageControl : MonoBehaviour, IDragHandler, IPointerClickHandler
     {
-        [SerializeField] Image PickerImage;
+        [SerializeField] private Image PickerImage;
 
         private RawImage SVImage;
-
         private ColorPickerControl CC;
         private RectTransform rect;
         private RectTransform pickerTransform;
-
 
         private void Awake()
         {
@@ -26,47 +22,41 @@ namespace Bozo.ModularCharacters
             rect = GetComponent<RectTransform>();
             pickerTransform = PickerImage.GetComponent<RectTransform>();
 
-            pickerTransform = PickerImage.GetComponent<RectTransform>();
-            pickerTransform.position = new Vector2(-(rect.sizeDelta.x * 0.5f), -(rect.sizeDelta.y * 0.5f));
+            // Initialize pickerTransform to bottom-left corner
+            pickerTransform.localPosition = new Vector2(-(rect.sizeDelta.x * 0.5f), -(rect.sizeDelta.y * 0.5f));
         }
 
         private void UpdateColor(PointerEventData eventData)
         {
-            Vector3 pos = rect.InverseTransformPoint(eventData.position);
-
-            float deltaX = rect.sizeDelta.x * 0.5f;
-            float deltaY = rect.sizeDelta.y * 0.5f;
-
-            if (pos.x < -deltaX)
+            if (eventData.pressEventCamera == null)
             {
-                pos.x = -deltaX;
-            }
-            if (pos.x > deltaX)
-            {
-                pos.x = deltaX;
-            }
-            if (pos.y < -deltaY)
-            {
-                pos.y = -deltaY;
-            }
-            if (pos.y > deltaY)
-            {
-                pos.y = deltaY;
+                Debug.LogWarning("EventData pressEventCamera is null. Check XR Ray Interactor setup.");
+                return;
             }
 
-            float x = pos.x + deltaX;
-            float y = pos.y + deltaY;
+            if (RectTransformUtility.ScreenPointToLocalPointInRectangle(rect, eventData.position, eventData.pressEventCamera, out Vector2 localPoint))
+            {
+                float halfWidth = rect.sizeDelta.x * 0.5f;
+                float halfHeight = rect.sizeDelta.y * 0.5f;
 
-            float xNorm = x / rect.sizeDelta.x;
-            float YNorm = y / rect.sizeDelta.y;
+                localPoint.x = Mathf.Clamp(localPoint.x, -halfWidth, halfWidth);
+                localPoint.y = Mathf.Clamp(localPoint.y, -halfHeight, halfHeight);
 
-            pickerTransform.localPosition = pos;
+                pickerTransform.localPosition = localPoint;
 
-            PickerImage.color = Color.HSVToRGB(0, 0, 1 - YNorm);
+                float xNorm = (localPoint.x + halfWidth) / rect.sizeDelta.x;
+                float yNorm = (localPoint.y + halfHeight) / rect.sizeDelta.y;
 
-            CC.SetSV(xNorm, YNorm);
+                PickerImage.color = Color.HSVToRGB(0, 0, 1 - yNorm);
 
+                CC.SetSV(xNorm, yNorm);
+            }
+            else
+            {
+                Debug.LogWarning("Failed to convert screen point to local point.");
+            }
         }
+
         public void OnDrag(PointerEventData eventData)
         {
             UpdateColor(eventData);
