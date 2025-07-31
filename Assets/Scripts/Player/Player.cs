8f83de931b2872dab2de7422c59412b88ed5d881
Davin Lewis <102100872+davinlewis@users.noreply.github.com>
2025-07-31 16:37:34 -0700

    added settings manager with lots of accessibility settings

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -1,4 +1,9 @@
 using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+using UnityEngine.XR.Interaction.Toolkit.Interactors;
+using UnityEngine.XR.Interaction.Toolkit.Locomotion.Comfort;
+using UnityEngine.XR.Interaction.Toolkit.Locomotion.Movement;
+using UnityEngine.XR.Interaction.Toolkit.Locomotion.Turning;
 using UnityEngine.XR.Interaction.Toolkit.Samples.StarterAssets;
 
 public class Player : MonoBehaviour
@@ -19,6 +24,14 @@ public class Player : MonoBehaviour
     private DynamicMoveProvider _moveProvider;
 
     private float _initialSpeed;
+
+    TunnelingVignetteController _tunnelingVignetteController;
+    GameObject RightController;
+    GameObject LeftController;
+    NearFarInteractor _rightNearFarInteractor;
+    NearFarInteractor _leftNearFarInteractor;
+    ControllerInputActionManager rightControllerInputActionManager;
+    ContinuousMoveProvider continuousMoveProvider;
 
     private void Awake()
     {
@@ -44,6 +57,55 @@ public class Player : MonoBehaviour
         {
             Debug.LogError("Could not find hand objects in player hierarchy. Make sure they are named 'RightHand' and 'LeftHand' and are children of the XR Rig.");
         }
+
+        _tunnelingVignetteController = GetComponentInChildren<TunnelingVignetteController>();
+        RightController = transform.Find("XR Origin (XR Rig)/Camera Offset/Right Controller").gameObject;
+        LeftController = transform.Find("XR Origin (XR Rig)/Camera Offset/Left Controller").gameObject;
+        _rightNearFarInteractor = RightController.GetComponentInChildren<NearFarInteractor>();
+        _leftNearFarInteractor = LeftController.GetComponentInChildren<NearFarInteractor>();
+
+        rightControllerInputActionManager = RightController.GetComponent<ControllerInputActionManager>();
+        continuousMoveProvider = GetComponentInChildren<ContinuousMoveProvider>();
+    }
+
+    public void Start()
+    {
+        _tunnelingVignetteController.enabled = SettingsManager.Instance.TunnelingVignette;
+        _tunnelingVignetteController.defaultParameters.apertureSize = SettingsManager.Instance.TunnelingVignetteAperatureSize;
+        _tunnelingVignetteController.defaultParameters.featheringEffect = SettingsManager.Instance.TunnelingVignetteFeathering;
+
+        if (SettingsManager.Instance.ToggleGrab)
+        {
+            _rightNearFarInteractor.selectActionTrigger = XRBaseInputInteractor.InputTriggerType.Toggle;
+            _leftNearFarInteractor.selectActionTrigger = XRBaseInputInteractor.InputTriggerType.Toggle;
+        }
+        else
+        {
+            _rightNearFarInteractor.selectActionTrigger = XRBaseInputInteractor.InputTriggerType.StateChange;
+            _leftNearFarInteractor.selectActionTrigger = XRBaseInputInteractor.InputTriggerType.StateChange;
+        }
+
+        if (SettingsManager.Instance.RangedInteractors)
+        {
+            _rightNearFarInteractor.enableFarCasting = true;
+            _leftNearFarInteractor.enableFarCasting = true;
+        }
+        else
+        {
+            _rightNearFarInteractor.enableFarCasting = false;
+            _leftNearFarInteractor.enableFarCasting = false;
+        }
+
+        if(SettingsManager.Instance.SmoothTurning)
+        {
+            rightControllerInputActionManager.smoothTurnEnabled = true;
+        }
+        else
+        {
+            rightControllerInputActionManager.smoothTurnEnabled = false;
+        }
+
+        continuousMoveProvider.moveSpeed = SettingsManager.Instance.SmoothTurningSpeed;
     }
 
     public void DisableMovement()
---
+++ Assets/SettingsManager.cs
@@ -0,0 +1,85 @@
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class SettingsManager : MonoBehaviour
+{
+    public static SettingsManager Instance { get; private set; }
+
+    [Header("Audio Settings")]
+    [Range(0f, 100f)] public float MasterVolume = 100f;
+    [Range(0f, 100f)] public float MusicVolume = 100f;
+    [Range(0f, 100f)] public float SFXVolume = 100f;
+
+    [Header("Accessibility Settings")]
+    public bool TunnelingVignette = true;
+    [Range(0f, 1f)] public float TunnelingVignetteAperatureSize = .757f;
+    [Range(0f, 1f)] public float TunnelingVignetteFeathering = .291f;
+
+    public bool SmoothTurning = false;
+    [Range(60f, 300f)] public float SmoothTurningSpeed = 60f;
+
+
+    public bool ToggleGrab = false;
+    public bool RangedInteractors = false;
+
+    public AudioMixer audioMixer;
+    AudioMixerGroup MasterAudio;
+    AudioMixerGroup Music;
+    AudioMixerGroup SFX;
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(transform.parent);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+
+        AudioMixer audioMixer = Resources.Load<AudioMixer>("Audio/MainAudioMixer");
+        MasterAudio = audioMixer.FindMatchingGroups("Master")[0]; // Find the Master group in the audio mixer
+        Music = audioMixer.FindMatchingGroups("Music")[0]; // Find the Music group in the audio mixer
+        SFX = audioMixer.FindMatchingGroups("SFX")[0]; // Find the SFX group in the audio mixer
+    }
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void SetMasterVolume(float volume)
+    {
+        MasterVolume = volume;
+        float decibelVolume = VolumePercentToDecibel(MasterVolume);
+        MasterAudio.audioMixer.SetFloat("MasterVolume", decibelVolume);
+    }
+    public void SetMusicVolume(float volume)
+    {
+        MusicVolume = volume;
+        float decibelVolume = VolumePercentToDecibel(MusicVolume);
+        Music.audioMixer.SetFloat("MusicVolume", decibelVolume);
+    }
+
+    public void SetSFXVolume(float volume)
+    {
+        SFXVolume = volume;
+        float decibelVolume = VolumePercentToDecibel(SFXVolume);
+        SFX.audioMixer.SetFloat("SFXVolume", decibelVolume);
+    }
+
+    float VolumePercentToDecibel(float volumePercent)
+    {
+        float volume01 = Mathf.Clamp01(volumePercent / 100f);
+        return Mathf.Log10(volume01) * 20f;
+    }
+
+}
