0758f6e0fa5169bfad5c24005df9ab4d8bdd504d
Davin Lewis <102100872+davinlewis@users.noreply.github.com>
2025-08-05 12:49:51 -0700

    added main menu

---
+++ Assets/MainMenuButtons.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class MainMenuButtons : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Quit Game");
+        Application.Quit();
+    }
+}
