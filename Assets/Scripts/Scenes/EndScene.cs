bc6a8ac7ce402781a9751a1817dc66b280a83a76
Ben Bonus <jbenbonus@gmail.com>
2025-08-12 16:55:21 -0700

    Added some more end scene stuff

--- Assets/Scripts/Scenes/EndScene.cs
+++ Assets/Scripts/Scenes/EndScene.cs
@@ -4,8 +4,15 @@ public class EndScene : MonoBehaviour
 {
     [SerializeField] private GameObject _drunkDriverStayedPolaroid;
     [SerializeField] private GameObject _drunkDriverCrashedPolaroid;
+    [SerializeField] private GameObject _obituary;
+    [SerializeField] private GameObject _MiP;
+
     [SerializeField] private GameObject _phoneFoundPolaroid;
     [SerializeField] private GameObject _phoneLostPolaroid;
+
+    [SerializeField] private MeshRenderer _reportCard;
+    [SerializeField] private Material _goodGrades;
+    [SerializeField] private Material _badGrades;
 
     [SerializeField] private BoolValue _foundPhone;
 
@@ -14,6 +21,9 @@ public class EndScene : MonoBehaviour
         // Drunk driving outcome
         //_drunkDriverStayedPolaroid.SetActive(!GlobalStats.LetDrunkFriendDrive);
         _drunkDriverCrashedPolaroid.SetActive(GlobalStats.letDrunkFriendDrive);
+        _obituary.SetActive(!GlobalStats.called911);
+        _MiP.SetActive(GlobalStats.DrinkCount > 0);
+        _reportCard.material = GlobalStats.DrinkCount > 0 ? _badGrades : _goodGrades;
 
         // Phone outcome
         _phoneFoundPolaroid.SetActive(_foundPhone.Value);
