1c866d6de8007b0ce70fc7725acfcd43cb8b1707
Ben Bonus <jbenbonus@gmail.com>
2025-08-13 17:00:21 -0700

    Made obituary not show up when getting the drugged drink ending

--- Assets/Scripts/Scenes/Bonfire/BonfireScene.cs
+++ Assets/Scripts/Scenes/Bonfire/BonfireScene.cs
@@ -377,6 +377,8 @@ public class BonfireScene : MonoBehaviour
 
             await Player.Instance.loading.CloseEyesAsync(0.15f);
 
+            GlobalStats.called911 = true; // workaround to make obituary not show for this ending
+
             // Set new scene after eyes close
             Player.Instance.loading.LoadSceneByName("BonfireCutscene");
         });
