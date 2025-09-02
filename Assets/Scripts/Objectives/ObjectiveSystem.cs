e089f3d9a54346f9b396ca168cd1d3d2f67930cf
Ben Bonus <jbenbonus@gmail.com>
2025-09-02 11:04:53 -0700

    increased range on objective line error detection

--- Assets/Scripts/Objectives/ObjectiveSystem.cs
+++ Assets/Scripts/Objectives/ObjectiveSystem.cs
@@ -24,8 +24,8 @@ public class Objective
         path = new NavMeshPath();
 
         // Ensure both the start and end positions are on the NavMesh
-        position = NavMesh.SamplePosition(position, out NavMeshHit hit, 5f, NavMesh.AllAreas) ? hit.position : position;
-        point.position = NavMesh.SamplePosition(point.position, out hit, 5f, NavMesh.AllAreas) ? hit.position : point.position;
+        position = NavMesh.SamplePosition(position, out NavMeshHit hit, 8f, NavMesh.AllAreas) ? hit.position : position;
+        point.position = NavMesh.SamplePosition(point.position, out hit, 8f, NavMesh.AllAreas) ? hit.position : point.position;
 
         return NavMesh.CalculatePath(position, point.position, NavMesh.AllAreas, path);
     }
