91f9a219f2c470437ee0fc321e92440e9fdeada8
Ben Bonus <jbenbonus@gmail.com>
2026-01-13 16:46:21 -0800

    Added movement tutorial

--- Assets/VRProject/Scripts/Scenes/TutorialScene.cs
+++ Assets/VRProject/Scripts/Scenes/TutorialScene.cs
@@ -11,13 +11,38 @@ public class TutorialScene : MonoBehaviour
     [SerializeField] private AnimationClip wavingAnimation;
     [SerializeField] private AnimationClip idleAnimation;
 
+    // Misc
+    private Vector3 playerStartPos;
+
     public bool hasInteractedWithFriend { get; set; }
 
-    private async void Start()
+    private void Start()
     {
-        await UniTask.Delay(2500);
+        InitializeMovementTutorial();
+    }
 
-        TutorialButtons.Instance.HighlightButton(LeftControllerMaterialIndex.MENU_BUTTON);
+    private async void InitializeMovementTutorial()
+    {
+        playerStartPos = Player.Instance.Position;
+
+        await UniTask.Delay(15_000); // Time to check for player movement
+
+        // If player has not moved after some seconds, show tutorial text
+        if (Player.Instance.Position == playerStartPos)
+        {
+            Debug.Log("Player has not moved, showing tutorial text for movement.");
+            TutorialText.Instance.ShowText("Use the joystick to move around.");
+            TutorialButtons.Instance.HighlightButton(LeftControllerMaterialIndex.LEFT_JOYSTICK);
+
+            await UniTask.WaitUntil(() => Player.Instance.Position != playerStartPos);
+
+            TutorialText.Instance.HideText();
+            TutorialButtons.Instance.ResetButton(LeftControllerMaterialIndex.LEFT_JOYSTICK);
+        }
+        else
+        {
+            Debug.Log("Player has moved, no need to show movement tutorial.");
+        }
     }
 
     private void Update()
